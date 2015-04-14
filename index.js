9ae629959b605d9a59440cb7cf6a65e4b0dc9e6c
Matt Claypotch <thepotch@gmail.com>
2015-04-14 15:04:32 -0700

    first

---
+++ index.js
@@ -0,0 +1,131 @@
+/* jshint esnext: true */
+var parseString = avow(require('xml2js').parseString);
+var request = avow(require('request'), 1);
+var _ = require('lodash');
+var handlebars = require('handlebars');
+var fs = require('fs');
+var template = handlebars.compile(fs.readFileSync(__dirname + '/template.hb').toString());
+var express = require('express');
+var http = require('http');
+
+function avow(fn, pick) {
+  pick = pick || 0;
+  return function () {
+    var args = _.toArray(arguments);
+    return new Promise(function (resolve, reject) {
+      args.push(function (err) {
+        var results = _.toArray(arguments).slice(1);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results[pick]);
+        }
+      });
+      fn.apply(null, args);
+    });
+  };
+}
+
+var TOKEN = '14e73b26-e7d9-4064-9c60-2e0af41de20b';
+
+function paths(o, d, cb) {
+  console.error('connecting');
+  return request(
+    'http://services.my511.org/traffic/getpathlist.aspx?token=' + TOKEN + '&o=' + o + '&d=' + d
+  )
+  .then(parseString)
+  .then(_.partialRight(_.result, 'paths'))
+  .then(_.partialRight(_.result, 'path'));
+}
+
+
+//
+// request('http://services.my511.org/traffic/getoriginlist.aspx?token=14e73b26-e7d9-4064-9c60-2e0af41de20b')
+//   .then(parseString)
+//   .then(function (result) {
+//     result.origins.origin.forEach(function(o) {
+//       console.error('[' + o.node + '] ' + o.city + ' - ' + o.mainRoad + ' AT ' + o.crossRoad);
+//     });
+//   });
+
+/*
+, function (err, paths) {
+console.dir(paths);
+console.dir(paths[0].segments[0].segment);
+}
+*/
+
+var roadURLs = {
+  'CA': 'cahwy.svg',
+  'US': 'ushwy.svg',
+  'I': 'interstate.svg'
+};
+
+function condense(destination) {
+  return function (paths) {
+    console.error('processing');
+    paths = paths.map(function (p, i) {
+      var o = {
+        destination: destination
+      };
+      if (i === 0) {
+        o.heading = true;
+      }
+      // console.dir(p.segments[0].segment);
+      o.roads = p.segments[0].segment
+      .map(function (s) {
+        s =  _.result(s, 'road')[0];
+        var r = s.match(/^([A-Z]+)-([0-9]+)\s\w$/);
+        if (!r) {
+          return null;
+        }
+        var url = roadURLs[r[1]];
+        if (url) {
+          return {
+            full: s,
+            type: r[1],
+            num: r[2],
+            url: url
+          };
+        }
+      }).filter(function (s) {
+        return !!s;
+      });
+      o.travelTime = p.currentTravelTime[0];
+      o.avgTravelTime = p.typicalTravelTime[0];
+      return o;
+    });
+    return paths;
+  };
+}
+
+var doc;
+
+function update() {
+  Promise.all([
+    paths(35,42).then(condense('San Francisco')),
+    paths(35, 460).then(condense('San Jose')),
+    paths(35, 637).then(condense('Los Gatos')),
+    paths(35, 1285).then(condense('Fremont'))
+    ]).then(function (paths) {
+      paths = _.flatten(paths);
+      console.error('writing');
+      doc = template({paths: paths});
+    }).catch(console.error.bind(console));
+}
+setInterval(update, 5 * 60 * 1000);
+update();
+
+
+var app = express();
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/static'));
+var server = http.createServer(app);
+
+app.get('/', function(req, res) {
+  res.end(doc);
+});
+
+server.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
---
+++ template.hb
@@ -0,0 +1,131 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href='http://fonts.googleapis.com/css?family=Fira+Mono|Raleway:500' rel='stylesheet' type='text/css'>
+    <style>
+      table {
+        width: 100%;
+      }
+      body {
+        background: #222;
+        color: #fff;
+        font-size: 4vmin;
+        font-family: 'Raleway', sans-serif;
+      }
+      .route i {
+        display: inline-block;
+        vertical-align: middle;
+        font-size: .5em;
+        margin-right: 1em;
+        letter-spacing: .1em;
+        font-style: normal;
+        text-transform: uppercase;
+      }
+      .road {
+        position: relative;
+        display: inline-block;
+        vertical-align: middle;
+        width: 6vmin;
+        height: 1.2em;
+        text-align: center;
+      }
+      .road img {
+        height: 4.5vmin;
+      }
+      .road span {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform:translate(-50%, -50%);
+        content:attr(data-num);
+        color: #fff;
+        display: block;
+        font-size: 2vmin;
+        font-family: sans-serif;
+        line-height: 1;
+      }
+      .road-CA span {
+        top: 3vmin;
+      }
+      .road-I span {
+        top: 2.6vmin;
+      }
+      .road-US span {
+        top: 2.5vmin;
+        color: #000;
+      }
+      ul {
+        list-style-type: none;
+        padding: 0 1em;
+      }
+      li {
+        text-align: center;
+        background: #222;
+        margin-top: .2em;
+      }
+      li > div {
+        text-align: left;
+        display: inline-block;
+      }
+      h1 {
+        font-family: 'Raleway';
+        text-transform: uppercase;
+        letter-spacing: .2em;
+        text-align: center;
+        font-size: 2em;
+        margin: .5em;
+        font-weight: 500;
+      }
+      .dest, .time {
+        line-height: 1.4em;
+        font-family: 'Fira Mono';
+        text-indent: 2px;
+        letter-spacing: 4px;
+        background: #000;
+        background-image:
+          linear-gradient(90deg, #222 5%, transparent 5%, transparent 95%, #222 95%),
+          linear-gradient(#222 10%, transparent 10%, transparent 50%, rgba(100,100,100,.2) 50%, rgba(100,100,100,.2) 90%, #222 90%);
+        background-size: calc(1ch + 4px) 50px;
+        background-position: left -.1em;
+      }
+      .dest {
+        width: calc(16ch + 64px);
+      }
+      .route {
+        width: 25vw;
+        margin: 0 4vw;
+      }
+      .time {
+        text-indent: -5px;
+        width: calc(10ch + 40px);
+        text-align: right;
+      }
+    </style>
+  </head>
+  <body>
+      <header>
+        <h1>Mountain View Transit</h1>
+      </header>
+      <ul>
+        {{#each paths}}
+        <li>
+          <div class="dest">
+            {{destination}}
+          </div>
+          <div class="route">
+            <i>via</i>
+            {{#each roads}}
+            <div class="road road-{{type}}" title="{{full}}">
+              <img src="{{url}}" alt="{{full}}">
+              <span>{{num}}</span>
+            </div>
+            {{/each}}
+          </div>
+          <div class="time">{{travelTime}} mins</div>
+        </tr>
+        {{/each}}
+    </ul>
+  </body>
+</html>
